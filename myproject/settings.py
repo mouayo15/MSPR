a06bd1734986724836c26255bd4be522965b6344
MouAyo <ayoub.mouadil@outlook.com>
2024-09-17 13:36:22 +0200

    add swagger doc

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'rest_framework_swagger',
     'products',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
